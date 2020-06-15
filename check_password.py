11d1a701b704267451658cc477bd5cc755167aed
Ricardo Morato Rocha <ricardomorator@hotmail.com>
2020-06-14 23:09:12 -0300

    feat(hash): Imported hashlib.

--- check_password.py
+++ check_password.py
@@ -1,8 +1,11 @@
 import requests
+import hashlib
 
 url = 'https://api.pwnedpasswords.com/range/' + 'CBFDA'
 res = requests.get(url)
 
+
+print(hashlib.sha1('123'.encode('utf-8')).hexdigest())
 
 class CheckPassword():
   pass
